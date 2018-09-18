fde74164d9daff0569f4de8a64ffe6849ddb7b3d
yuriybon <yuriy_bon@mail.ru>
2018-09-18 09:43:47 +0200

    Add basic redirection and housekeep

--- src/main/java/ua/odessa/bondar/controller/RedirectController.java
+++ src/main/java/ua/odessa/bondar/controller/RedirectController.java
@@ -1,14 +1,14 @@
 package ua.odessa.bondar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.odessa.bondar.domain.User;
 import ua.odessa.bondar.repo.UserRepository;
 import ua.odessa.bondar.service.UserService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -19,10 +19,6 @@ public class RedirectController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    //@ApiIgnore
     @GetMapping("/swagger")
     public void swagger(HttpServletResponse resp) throws IOException {
         resp.sendRedirect("/swagger-ui.html");
@@ -33,15 +29,6 @@ public class RedirectController {
         Iterable<User> list = userService.getUsers();
         list.forEach((t) -> System.out.println(t.getFirstName()));
         return (List<User>) list;
-        //resp.sendRedirect("/UserView.html");
-    }
-
-    @GetMapping("/api/v1/userRepList")
-    public List<User> userRepList(HttpServletResponse resp) throws IOException {
-        List<User> list = userRepository.findAll();
-        list.forEach((t) -> System.out.println(t.getFirstName()));
-        return list;
-        //resp.sendRedirect("/UserView.html");
     }
 
     @GetMapping("/edit")
@@ -53,5 +40,27 @@ public class RedirectController {
     @GetMapping("/view")
     public void viewTable(HttpServletResponse resp) throws IOException {
         resp.sendRedirect("/UserView.html");
+    }
+
+    @PostMapping("/api/v1/user/{userId}/delete")
+    public void deleteUser(@PathVariable("userId") Long userId) throws IOException {
+        userService.deleteUser(userId);
+    }
+
+    @PostMapping("/api/v1/user/create")
+    public void createUser(@RequestParam String firstName
+                         , @RequestParam String lastName
+                         , @RequestParam Date birthDay
+                         , @RequestParam String gender) throws IOException {
+        userService.createUser(firstName,lastName,birthDay,gender);
+    }
+
+    @PostMapping("/api/v1/user/update")
+    public void createUser(@RequestParam Long userId
+            , @RequestParam String firstName
+            , @RequestParam String lastName
+            , @RequestParam Date birthDay
+            , @RequestParam String gender) throws IOException {
+        userService.updateUser(userId,firstName,lastName,birthDay,gender);
     }
 }
