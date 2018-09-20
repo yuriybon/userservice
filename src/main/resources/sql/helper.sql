b79bf8c0a11046a49d2352c119de2aab4f857c4d
yuriybon <yuriy_bon@mail.ru>
2018-09-20 08:10:44 +0200

    helper sql add a few lines to db

--- src/main/resources/sql/helper.sql
+++ src/main/resources/sql/helper.sql
@@ -8,6 +8,14 @@ GENDER varchar(255)
 
 insert into users(FIRST_NAME, LAST_NAME, BIRTH_DATE,GENDER)
 values('Bill', 'Gates','28-OCT-1955','MEN');
+
+insert into users(FIRST_NAME, LAST_NAME, BIRTH_DATE,GENDER)
+values('Larry', 'Elison','01-NOV-1952','MEN');
+
+insert into users(FIRST_NAME, LAST_NAME, BIRTH_DATE,GENDER)
+values('Big', 'Boss','01-JUN-1971','MEN');
+
 commit;
+
 
 select * from users;
